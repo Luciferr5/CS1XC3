fec4faac0fc0b114bb7ec0655f90dc9508f5cff2
saxenp4 <saxenp4@pascal.cas.mcmaster.ca>
2023-02-17 23:21:28 -0500

    Lab 4

--- L04/lab4.sh
+++ L04/lab4.sh
@@ -4,10 +4,48 @@
 WDIR="$1"
 cd "$WDIR"
 
+
+F="file1.txt"
+
 if [ "$2" -eq 1 ] ; then
-    echo "TODO Objective 1"
+    if [ ! -f "$F" ]; then
+        touch "$F"
+        echo $USER > "$F"
+    fi
+
 elif [ "$2" -eq 2 ] ; then
-    echo "TODO Objective 2"
+    if [ -f "$F" ]; then    
+        if [ ! -d "$backup" ]; then 
+            mkdir "backup"
+        fi
+        cp "$F" "./backup"
+    fi
+
+
+elif [ "$2" -eq 3 ] ; then
+    if [ -f "backup/$F" ]; then
+        cp backup/$F $F
+        rm backup/$F
+    fi
+
+
+
+elif [ $# -ge 3 ]; then 
+    F="$3"
+else
+   F="file1.txt" 
 fi
 
-echo "Finished"
+
+
+if [ $# -gt 3 ]; then
+    if [ -f "excess.txt" ]; then
+        rm excess.txt
+        touch excess.txt
+    fi
+    shift 3
+
+    for j in "$@"; do
+        echo "$j" >> "excess.txt" 
+    done 
+fi
